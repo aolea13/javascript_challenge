7b7b000018e5d0db708151e829859851aa03bff6
aolea13 <austinolea@gmail.com>
2021-03-24 13:58:05 -0600

    :sparkles: level_1 up to events

---
+++ UFO_level_1/level_1.js
@@ -0,0 +1,18 @@
+// Data Table Conect
+var tableData = data;
+
+// Events
+var tbody = d3.select('tbody');
+var buttons = d3.select('#filter-btn');
+var inputfield1 = d3.select('#datetitme');
+var inputfield2 = d3.select('#city');
+var restbtn = d3.select('#reset-btn');
+var columns = ['datetime', 'city', 'state', 'country', 'shape', 'durationMinutes', 'comments']
+
+var populate = (datainput) => {
+    datainput.forEach(ufo_sightings => {
+        var row = tbody.append('tr');
+        columns.forEach(column => row.append('td').text(ufo_sightings[column])
+        )
+    });
+}
